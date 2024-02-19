02d6a9319f50b5b4cd2f019dcb00cd87a717f10d
webdesignervinayak <71724290+webdesignervinayak@users.noreply.github.com>
2024-02-20 02:34:53 +0530

    cartslice update

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -19,10 +19,8 @@ const cartSlice = createSlice ({
             const itemIndex = state.items.findIndex(item => item.card.info.id == action.payload.card.info.id)
             if(itemIndex >= 0)
             state.items[itemIndex].itemQuantity -= 1;
-            if(state.items[itemIndex].itemQuantity == 0){
-            const tempItem = {...action.payload,itemQuantity: 1}
+            if(state.items[itemIndex].itemQuantity == 0)
             state.items.splice(itemIndex,1);
-            }
             /*const itemId = action.payload.card.info.id;
             state.items = state.items.filter((item) => item.card.info.id !== itemId);*/
         },
